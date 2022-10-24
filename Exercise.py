1a8469daefe6244a058a4b12952f7ea6b726193a
Bazsmith <bazsmith@me.com>
2022-10-24 12:16:07 +0100

    my new exercise

---
+++ Exercise.py
@@ -0,0 +1,23 @@
+print("This is a new line")
+
+def newsub():
+    """"THis is my sub"""
+    print("Hi")
+    print("the cat sat on the mat")
+    print("the dog chased the cat")
+
+newsub()
+
+import random
+print(random.randint(0,80))
+
+
+
+
+
+
+
+
+
+
+
